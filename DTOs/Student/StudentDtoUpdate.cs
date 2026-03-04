20974e5595c2463bcf795768d91d00fbed300a6d
Foster Amponsah <foster.asante@gwosevo.com>
2026-03-04 12:46:36 +0000

    Update

--- DTOs/Student/StudentDtoUpdate.cs
+++ DTOs/Student/StudentDtoUpdate.cs
@@ -8,10 +8,14 @@ namespace student_management_api.DTOs.Student
 {
     public class StudentDtoUpdate
     {
-        [Required(ErrorMessage = "Name is required")]
+        [Required(ErrorMessage = "First name is required")]
         [StringLength(50, MinimumLength = 2, ErrorMessage = "First name must be between 2 and 50 characters")]
         public string? FirstName { get; set; }
+        [Required(ErrorMessage = "Last name is required")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Last name must be between 2 and 50 characters")]
         public string? LastName { get; set; }
+        [Required(ErrorMessage = "PendingFees is required")]
+        [Range(1, 200, ErrorMessage = "Enter correct value")]
         public decimal PendingFees { get; set; }
     }
 }
