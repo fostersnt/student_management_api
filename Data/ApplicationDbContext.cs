ae6e1e896832bacf03fa3be6caf53e2612b187ed
Foster Amponsah <foster.asante@gwosevo.com>
2026-03-16 11:03:06 +0000

    Update

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -11,9 +11,9 @@ namespace student_management_api.Models.Data
 {
     public class ApplicationDbContext : IdentityDbContext<User, Role, int>
     {
-        public ApplicationDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
         {
-            
+
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
@@ -23,13 +23,27 @@ namespace student_management_api.Models.Data
             // Rename tables
             // builder.Entity<User>().ToTable("Users");
             // builder.Entity<Role>().ToTable("Roles");
-            builder.Entity<IdentityUserRole<Guid>>().ToTable("UserRoles");
-            builder.Entity<IdentityUserClaim<Guid>>().ToTable("UserClaims");
-            builder.Entity<IdentityUserLogin<Guid>>().ToTable("UserLogins");
-            builder.Entity<IdentityRoleClaim<Guid>>().ToTable("RoleClaims");
-            builder.Entity<IdentityUserToken<Guid>>().ToTable("UserTokens");
-        }
+            builder.Entity<IdentityUserRole<int>>().ToTable("UserRoles");
+            builder.Entity<IdentityUserClaim<int>>().ToTable("UserClaims");
+            builder.Entity<IdentityUserLogin<int>>().ToTable("UserLogins");
+            builder.Entity<IdentityRoleClaim<int>>().ToTable("RoleClaims");
+            builder.Entity<IdentityUserToken<int>>().ToTable("UserTokens");
 
+            builder.Entity<Role>().HasData(
+        new Role
+        {
+            // Id = 1,
+            Name = "Admin",
+            NormalizedName = "ADMIN"
+        },
+        new Role
+        {
+            // Id = 2,
+            Name = "User",
+            NormalizedName = "USER"
+        }
+    );
+        }
 
         public DbSet<Student> Students { get; set; }
         public DbSet<Book> Books { get; set; }
