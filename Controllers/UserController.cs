e8dbf1c3587fcb2af55ac5d639c08d551c6b1e2e
Foster Amponsah <foster.asante@gwosevo.com>
2026-03-19 11:56:15 +0000

    Update

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using student_management_api.DTOs.User;
 using student_management_api.Repository.IService;
@@ -9,6 +10,7 @@ using student_management_api.Repository.Service;
 
 namespace student_management_api.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/user")]
     public class UserController : ControllerBase
